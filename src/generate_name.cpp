42bf74129cdb825f690e1b1a99eb1be2a7c10e5b
Ivan Kablukov <ikablukov@ac-sw.com>
2011-12-02 17:27:09 +0600

    Lemmas' names generator
    Change-Id: Ibc6e16caa92cc5da27872754f49de5b0bba62d73

--- src/generate_name.cpp
+++ src/generate_name.cpp
@@ -13,16 +13,10 @@
 #include "llir.h"
 
 
+///preconditions
+
 std::wstring NameGenerator::makeNamePredicatePrecondition(ir::Predicate &_predicate){
     return _predicate.getName() + L"Precondition";
-}
-
-std::wstring NameGenerator::makeNamePredicatePostcondition(ir::Predicate &_predicate){
-    return _predicate.getName() + L"Postcondition";
-}
-
-std::wstring NameGenerator::makeNamePredicateMeasure(ir::Predicate &_predicate){
-    return _predicate.getName() + L"Measure";
 }
 
 std::wstring NameGenerator::makeNamePredicateBranchPrecondition(ir::Predicate &_predicate, size_t _branchNumber){
@@ -40,6 +34,20 @@ std::wstring NameGenerator::makeNameTypeBranchPreCondition(size_t _branchNumber)
 std::wstring NameGenerator::makeNameProcessBranchPreCondition(ir::Process &_process, size_t _branchNumber){
     return _process.getName() + fmtInt(_branchNumber, L"Precondition%u");
 }
+
+
+///verification
+
+std::wstring NameGenerator::makeNamePredicatePostcondition(ir::Predicate &_predicate){
+    return _predicate.getName() + L"Postcondition";
+}
+
+std::wstring NameGenerator::makeNamePredicateMeasure(ir::Predicate &_predicate){
+    return _predicate.getName() + L"Measure";
+}
+
+
+///submodules
 
 std::wstring NameGenerator::makeNameSubmoduleForType(){
     m_typeNumber++;
@@ -61,4 +69,58 @@ std::wstring NameGenerator::makeNameSubmoduleForProcess(ir::Process &_process){
 
 std::wstring NameGenerator::makeNameSubmoduleForPredicate(ir::Predicate &_predicate){
     return _predicate.getName();
+}
+
+
+
+///semantics' lemmas
+
+std::wstring NameGenerator::makeNameLemmaCall(){
+    m_callNumber++;
+    return fmtInt(m_callNumber, L"Call%u");
+}
+
+std::wstring NameGenerator::makeNameLemmaSwitchDefault(){
+    m_switchDefaultNumber++;
+    return fmtInt(m_switchDefaultNumber, L"SwitchDefault%u");
+}
+
+std::wstring NameGenerator::makeNameLemmaSwitchCase(){
+    m_switchCaseNumber++;
+    return fmtInt(m_switchCaseNumber, L"SwitchCase%u");
+}
+
+std::wstring NameGenerator::makeNameLemmaArrayPartIndex(){
+    m_arrayPartIndexNumber++;
+    return fmtInt(m_arrayPartIndexNumber, L"ArrayPartIndex%u");
+}
+
+std::wstring NameGenerator::makeNameLemmaSubtypeParam(){
+    m_subtypeParamNumber++;
+    return fmtInt(m_subtypeParamNumber, L"SubtypeParam%u");
+}
+
+std::wstring NameGenerator::makeNameLemmaUnionConsField(){
+    m_unionConsFieldNumber++;
+    return fmtInt(m_unionConsFieldNumber, L"UnionConsField%u");
+}
+
+std::wstring NameGenerator::makeNameLemmaAssignment(){
+    m_assignmentNumber++;
+    return fmtInt(m_assignmentNumber, L"Assignment%u");
+}
+
+std::wstring NameGenerator::makeNameLemmaDivide(){
+    m_divideNumber++;
+    return fmtInt(m_divideNumber, L"Divide%u");
+}
+
+std::wstring NameGenerator::makeNameLemmaArrayCons(){
+    m_arrayConsNumber++;
+    return fmtInt(m_arrayConsNumber, L"ArrayConstructor%u");
+}
+
+std::wstring NameGenerator::makeNameLemmaArrayMod(){
+    m_arrayModNumber++;
+    return fmtInt(m_arrayModNumber, L"ArrayModification%u");
 }
--- src/generate_name.h
+++ src/generate_name.h
@@ -17,24 +17,49 @@
 
 struct NameGenerator{
 private:
-    size_t m_typeNumber;
+    size_t m_typeNumber;   //for naming modules
     size_t m_lambdaNumber;
+    size_t m_callNumber;   //for naming lemmas
+    size_t m_switchDefaultNumber;
+    size_t m_switchCaseNumber;
+    size_t m_arrayPartIndexNumber;
+    size_t m_subtypeParamNumber;
+    size_t m_unionConsFieldNumber;
+    size_t m_assignmentNumber;
+    size_t m_divideNumber;
+    size_t m_arrayConsNumber;
+    size_t m_arrayModNumber;
 
 public:
-    NameGenerator() : m_typeNumber(0), m_lambdaNumber(0) {}
+    NameGenerator() : m_typeNumber(0), m_lambdaNumber(0), m_callNumber(0), m_switchDefaultNumber(0),
+                      m_switchCaseNumber(0), m_arrayPartIndexNumber(0), m_subtypeParamNumber(0), m_unionConsFieldNumber(0),
+                      m_assignmentNumber(0), m_divideNumber(0), m_arrayConsNumber(0), m_arrayModNumber(0) {}
 
-    std::wstring makeNamePredicatePrecondition(ir::Predicate &_predicate);
-    std::wstring makeNamePredicatePostcondition(ir::Predicate &_predicate);
-    std::wstring makeNamePredicateMeasure(ir::Predicate &_predicate);
+    std::wstring makeNameSubmoduleForType();     //for modules
+    std::wstring makeNameSubmoduleForLambda();
+    std::wstring makeNameSubmoduleForProcess(ir::Process &_process);
+    std::wstring makeNameSubmoduleForPredicate(ir::Predicate &_predicate);
+
+    std::wstring makeNamePredicatePrecondition(ir::Predicate &_predicate);    //for preconditions
     std::wstring makeNamePredicateBranchPrecondition(ir::Predicate &_predicate, size_t _branchNumber);
     std::wstring makeNameTypePreCondition();
     std::wstring makeNameTypeBranchPreCondition(size_t _branchNumber);
     std::wstring makeNameProcessBranchPreCondition(ir::Process &_process, size_t _branchNumber);
-    std::wstring makeNameSubmoduleForType();
-    std::wstring makeNameSubmoduleForLambda();
-    std::wstring makeNameSubmoduleForProcess(ir::Process &_process);
-    std::wstring makeNameSubmoduleForPredicate(ir::Predicate &_predicate);
-    std::wstring makeNameLambdaToPredicate();
+    std::wstring makeNameLambdaToPredicate();   //for lambdas
+
+    std::wstring makeNamePredicatePostcondition(ir::Predicate &_predicate);     //for verification
+    std::wstring makeNamePredicateMeasure(ir::Predicate &_predicate);
+
+    std::wstring makeNameLemmaCall();     //for lemmas
+    std::wstring makeNameLemmaSwitchDefault();
+    std::wstring makeNameLemmaSwitchCase();
+    std::wstring makeNameLemmaArrayPartIndex();
+    std::wstring makeNameLemmaSubtypeParam();
+    std::wstring makeNameLemmaUnionConsField();
+    std::wstring makeNameLemmaAssignment();
+    std::wstring makeNameLemmaDivide();
+    std::wstring makeNameLemmaArrayCons();
+    std::wstring makeNameLemmaArrayMod();
 };
 
 #endif /* GENERATE_NAME_H_ */
