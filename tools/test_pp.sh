45005099ab1a1cf0ca05d16781730a3cb54e46c9
Chushkin Mikhail <math.h@yandex.ru>
2011-12-19 13:36:14 +0600

    Add tests for P-syntax pretty printer.
    Change-Id: I8a47853c3ecd58a254f992e09fe041b182c48782

---
+++ tools/test_pp.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+CUR_DIR=$1
+BIN_DIR=$2
+TEST=$3
+
+shift 3
+echo "[ Test   ]" `basename $TEST`
+
+ORIG=`mktemp`
+PP=`mktemp`
+TEMP=`mktemp`
+NEWTEST=`mktemp`
+
+$BIN_DIR/plang -pflat $@ $TEST > $ORIG
+$BIN_DIR/plang -pp $@ $TEST > $TEMP
+$BIN_DIR/plang -pflat $@ $TEMP > $PP
+
+DIFFRESULT=`diff $ORIG $PP`
+rm $ORIG $TEMP $PP $NEWTEST
+
+if test -z $DIFFRESULT
+then
+    echo "[ \033[0;32mPassed\033[0m ]"
+    exit 0
+else
+    echo "[ \033[0;31mFailed\033[0m ]" $DIFFRESULT
+    exit 1
+fi
