7c15be8dff5c1b97b39b2740078f2e094cc2e25c
Nikita Karnauhov <jinx@ac-sw.com>
2012-03-23 12:07:00 +0700

    Implement aliasing of specified same-typed objects when cloning.
    Change-Id: I46f51166b77b365117f537ff1470ac77dca7a306

--- src/autoptr.cpp
+++ src/autoptr.cpp
@@ -2,6 +2,7 @@
 ///
 
 #include "autoptr.h"
+#include "utils.h"
 
 std::stack<void *> Counted::m_allocs;
 
@@ -64,7 +65,7 @@ void *Cloner::allocate(size_t _cSize, const void *_pOriginal) {
     Counted *pCounted = new(true) Counted(::operator new(_cSize));
 
     Counted::ref(pCounted);
-    m_cache[_pOriginal] = pCounted;
+    m_cache[_getHandle(_pOriginal)] = pCounted;
 
     return (void *)pCounted->m_pCountedPtr;
 }
@@ -73,13 +74,42 @@ void *Cloner::allocate(size_t _cSize, const Counted *_pOriginal) {
     Counted *pCounted = (Counted *)Counted::operator new(_cSize, true);
 
     Counted::ref(pCounted);
-    m_cache[_pOriginal] = pCounted;
+    m_cache[_getHandle(_pOriginal)] = pCounted;
 
     return (void *)pCounted->m_pCountedPtr;
 }
 
+int Cloner::_getHandle(const void *_pObject) {
+    std::pair<Handles::iterator, bool> handle = m_handles.insert(
+        std::make_pair(_pObject, (int)m_handles.size()));
+
+    if (handle.second)
+        m_objects.insert(std::make_pair(handle.first->second, _pObject));
+
+    return handle.first->second;
+}
+
+void Cloner::_mergeHandles(int _nHandle, int _nOther) {
+    if (_nHandle == _nOther)
+        return;
+
+    std::pair<Objects::iterator, Objects::iterator> bounds =
+        m_objects.equal_range(_nOther);
+
+    for (Objects::iterator i = bounds.first; i != bounds.second;) {
+        Objects::iterator iNext = ::next(i);
+        const void *pObject = i->second;
+
+        m_handles[pObject] = _nHandle;
+        m_objects.erase(i);
+        m_objects.insert(std::make_pair(_nHandle, pObject));
+        i = iNext;
+    }
+}
+
 void *operator new(size_t _cSize, Cloner &_cloner, const void *_pOriginal) {
-    Cloner::Cache::iterator iObj = _cloner.m_cache.find(_pOriginal);
+    Cloner::Cache::iterator iObj = _cloner.m_cache.find(
+        _cloner._getHandle(_pOriginal));
     assert(iObj != _cloner.m_cache.end());
     return (void *)iObj->second;
 }
