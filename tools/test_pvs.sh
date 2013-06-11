f6cb23196ec611664dbc617919167faa7ef61633
Chushkin Mikhail <math.h@yandex.ru>
2013-06-11 12:41:49 +0700

    Add tests for backend to PVS.
    Change-Id: Id037d10f94a1cb12af87180069e30b58cb1e6a20

---
+++ tools/test_pvs.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+CUR_DIR=$1
+BIN_DIR=$2
+TEST=$3
+
+shift 3
+echo "[ Test   ]" `basename $TEST`
+
+EXPECTED=$TEST.pvs
+RESULT=`mktemp`
+
+$BIN_DIR/plang -b3 $TEST |sort > $RESULT
+
+sort $EXPECTED| diff -b -B $RESULT - >/dev/null 2>&1
+
+if [ "$?" = "0" ]; then
+    echo "[ \033[0;32mPassed\033[0m ]"
+    rm $RESULT
+    exit 0
+else
+    echo "[ \033[0;31mFailed\033[0m ]"
+    diff -u $RESULT $EXPECTED
+    rm $RESULT
+    exit 1
+fi
